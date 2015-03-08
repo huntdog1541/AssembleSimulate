c6cdf6cac52fab87d22d39e81be00d83bc52653b
David Hunt <huntdog1541@gmail.com>
2015-03-07 22:13:00 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by David on 3/7/2015.
+ */
+public class Main {
+
+    public Main()
+    {
+
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
