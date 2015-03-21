e02f365e712c18112012b37a9fc0a1573291493f
David Hunt <huntdog1541@gmail.com>
2015-03-21 18:23:49 -0500

    update files

--- Main.java
+++ Main.java
@@ -13,7 +13,6 @@ public class Main {
     public static void main(String args[])
     {
         VM virt = new VM();
-        System.out.println("Hello World");
         Register reg = new Register(57);
         JFrame frame = new JFrame ("Assemble Simulator");
         frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
